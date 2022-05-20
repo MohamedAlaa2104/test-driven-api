15b709273bef2c3e9c25509d463ca885b8302644
Mohamed Alaa <mohamedalaa2104@gmail.com>
2022-05-20 03:26:00 +0200

    Converting route resource to api resource for todo list

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\TodoListController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('todo-list', TodoListController::class);
+Route::apiResource('todo-list', TodoListController::class);
